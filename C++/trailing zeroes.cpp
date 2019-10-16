0500931b92d7829dcdbf1b7d6ae95cff44d27027
Soumya Rao <47022604+wowrao17@users.noreply.github.com>
2019-10-16 20:20:41 +0530

    edited trailing zeroes

--- C++/trailing zeroes.cpp
+++ C++/trailing zeroes.cpp
@@ -1,16 +1,19 @@
 //WAP to find the no. of trailing zeroes in decimal representation of n!
 #include<iostream>
 using namespace std;
-int main()
-{
-	int n,ans;
-	cout<<"enter a no."<<endl;
-	cin>>n;
-	while(n!=0)
+void trailingZeroes(int n)
+{        while(n!=0)
 	{
 		ans+=n/5;
 		n/=5;
 	}
 	cout<<"the trailing zeroes are"<<ans;
-	return 0;
+}
+
+int main()
+{
+	int n,ans;
+	cout<<"enter a no."<<endl;
+	cin>>n;
+
 }
