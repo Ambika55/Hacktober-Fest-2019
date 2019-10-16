b20df2ace7d0ca6f9c1571fc2b0214b8c133536c
Soumya Rao <47022604+wowrao17@users.noreply.github.com>
2019-10-16 20:13:48 +0530

    Added another function of insertion sort using recursion

--- C++/InsertionSort.cpp
+++ C++/InsertionSort.cpp
@@ -16,14 +16,40 @@ void InsertionSort(int arr[], int size)
         arr[j]= v;
     }
 }
-
+void insertionSortRecursive(int arr[], int n) 
+{ 
+    // Base case 
+    if (n <= 1) 
+        return; 
+  
+    // Sort first n-1 elements 
+    insertionSortRecursive( arr, n-1 ); 
+  
+    // Insert last element at its correct position 
+    // in sorted array. 
+    int last = arr[n-1]; 
+    int j = n-2; 
+  
+    /* Move elements of arr[0..i-1], that are 
+      greater than key, to one position ahead 
+      of their current position */
+    while (j >= 0 && arr[j] > last) 
+    { 
+        arr[j+1] = arr[j]; 
+        j--; 
+    } 
+    arr[j+1] = last; 
+} 
 int main()
 {
-    int arr[20], size,i;
+    int arr[20], size,i,n;
     cin>>size;
     for(i=0; i<size; i++)
         cin>>arr[i];
     InsertionSort(arr,size);
+    cout<<"Size of the array"<<endl;
+    cin>>n;
+    insertionSortRecursive(arr,n);
     cout<<"\n After Sorting the Array : ";
     for(i=0; i<size; i++)
         cout<<arr[i]<<" ";
