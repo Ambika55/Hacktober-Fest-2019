9ea1e10b5e55f3dd938f2ed97a6f4f522056e461
Soumya Rao <47022604+wowrao17@users.noreply.github.com>
2019-10-16 20:18:46 +0530

    Selection sort using recursion

--- C++/SelectionSort.cpp
+++ C++/SelectionSort.cpp
@@ -18,6 +18,23 @@ void SelectionSort(int arr[], int size)
 
     }
 }
+void recurSelectionSort(int a[], int n, int index = 0) 
+{ 
+    // Return when starting and size are same 
+    if (index == n) 
+       return; 
+  
+    // calling minimum index function for minimum index 
+    int k = minIndex(a, index, n-1); 
+  
+    // Swapping when index nd minimum index are not same 
+    if (k != index) 
+       swap(a[k], a[index]); 
+  
+    // Recursively calling selection sort function 
+    recurSelectionSort(a, n, index + 1); 
+} 
+  
 
 int main()
 {
@@ -25,8 +42,13 @@ int main()
     cin>>size;
     for(i=0; i<size; i++)
         cin>>arr[i];
+    for(i=0;i<n;i++)
+        cin>>a[i];
     SelectionSort(arr,size);
     cout<<"\n After Sorting the Array : ";
     for(i=0; i<size; i++)
         cout<<arr[i]<<" ";
+    cout<<"\n After Sorting the Array recursively : ";
+    for(i=0; i<n; i++)
+        cout<<a[i]<<" ";
 }
