5bd06bc6cbee12f43b5d4e47a7562460936100d9
Soumya Rao <47022604+wowrao17@users.noreply.github.com>
2019-10-24 13:38:42 +0530

    Updated Program
    This program gives the sum of the numbers, input by the user.

--- Python/helloWorld.py
+++ Python/helloWorld.py
@@ -1,1 +1,6 @@
-print("Hello World")
+print("Hello World, enter two numbers and I will give their sum as output")
+number1: input("First Number")
+number2: input("Second Number")
+
+sum= number1+number2
+print(sum)
